d7513b8eb4013a911974760bf629f8971813e813
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-23 19:11:30 +0530

    Update tasks remaining

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
 
+  const tasksNoun = taskList.length === 1 ? "task" : "tasks";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -40,7 +43,7 @@ function App(props) {
           <span className="visually-hidden"> tasks</span>
         </button> */}
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
