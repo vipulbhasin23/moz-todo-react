2d968dbf1d9e8e0dd45b410c257c2915318507c3
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-23 11:39:22 +0530

    Add unique keys to each iterated element

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import Todo from "./components/Todo";
 
 function App(props) {
   const taskList = props.tasks?.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} />
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
   ));
 
   return (
