bda7f51c44cd579e474280f8b08d7d608ad8e1f6
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-28 10:26:18 +0530

    Add toggleTaskCompleted

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import { useState } from "react";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
+  function toggleTaskCompleted(id) {
+    console.log(tasks[0]);
+  }
+
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
 
