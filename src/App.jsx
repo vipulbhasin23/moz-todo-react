b9c415a83f0be4ab75025823d2cbf2bf208aad71
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-08-01 11:12:24 +0530

    Add filter names and filter functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 import { useState } from "react";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState("All");
